67090509f002e9334571f6288e7ebd12694c93a1
shogokatoxx <syogo_br@yahoo.co.jp>
2019-06-04 07:59:19 +0000

    viewとsettings変更

--- simplechat/settings.py
+++ simplechat/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 LOGIN_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
